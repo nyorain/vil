13433168ef9b2772924713c7ed4fe0ff23927825
nyorain <nyorain@gmail.com>
2021-06-15 23:22:35 +0200

    Fix windows CI

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ echo on
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-meson build --backend=ninja --example=true --tests=true
+meson build --backend=ninja -Dexample=true -Dtests=true
 ninja -C build
 ninja -C build test
